d8dc5fb7850f89fff2f0c4c954fed1ac8b07d202
FacuFrancia <facundofrancia3@hotmail.com>
2021-06-09 18:15:57 -0300

    added playground for testing, change timer code, style and box timer

--- Templates/TimerHTML-JS/Timer.js
+++ Templates/TimerHTML-JS/Timer.js
@@ -1,12 +1,10 @@
 //Define vars to hold values
 let seconds = 0;
 let minutes = 0;
-let hours = 0;
 
 //Define vars to hold display value
 let displaySeconds = 0;
 let displayMinutes = 0;
-let displayHours = 0;
 
 //Define var to hold setInterval()
 let interval = null;
@@ -23,19 +21,14 @@ function StopWatch() {
         seconds = 0;
         minutes++;
 
-        if (minutes / 60 === 1) {
-            minutes = 0;
-            hours++;
-        }
     }
 
     //If seconds/minutes/hours are only one digit, add a leading 0
     (seconds < 10) ? displaySeconds = "0" + seconds.toString() : displaySeconds = seconds;
     (minutes < 10) ? displayMinutes = "0" + minutes.toString() : displayMinutes = minutes;
-    (hours < 10) ? displayHours = "0" + hours.toString() : displayHours = hours;
 
     //Display updated time values to user
-    document.getElementById("display").innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds;
+    document.getElementById("display").innerHTML = displayMinutes + ":" + displaySeconds;
 }
 
 
@@ -61,7 +54,7 @@ function reset() {
     window.clearInterval("interval");
     seconds = 0;
     minutes = 0;
-    hours = 0;
-    document.getElementById("display").innerHTML = "00:00:00";
+    document.getElementById("display").innerHTML = "00:00";
     document.getElementById("startStop").innerHTML = "start";
 }
+
