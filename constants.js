8d541abc1750a4873761064e40ddee62b755b2d7
FacuFrancia <facundofrancia3@hotmail.com>
2021-06-26 16:29:52 -0300

    Refactor code, new js archive with constants

---
+++ constants.js
@@ -0,0 +1,13 @@
+
+// Status Labels for buttons
+const Button_Labels = {
+    Start: "Start",
+    Stop: "Stop",
+    Reset: "Reset"
+}
+
+// Define variables to hold stopwatch status
+const Status_Types = {
+    STOPPED: 0,
+    STARTED: 1
+}
--- index.js
+++ index.js
@@ -1,27 +1,15 @@
+
 // Assigns onclick event to button elements in html
 const starTimerBtn = document.getElementById("startStop");
 const resetTimerBtn = document.getElementById("reset");
-
-// Calls the functions when the users clicks the buttons
-starTimerBtn.onclick = startStop;
-resetTimerBtn.onclick = reset;
-
+const startStopButton = document.getElementById("startStop");
+const counterElement = document.getElementById("counterElement");
 //Define variables to hold values
 let seconds = 0;
 let minutes = 0;
 
-//Define variables to hold setInterval()
-let interval = null;
-
-// Define variables to hold stopwatch status
-const Status_Types = {
-    STOPPED: 0,
-    STARTED: 1
-}
-let status = Status_Types.STOPPED;
-
 //StopWatch Function(logic)
-function StopWatch() {
+const StopWatch = () => {
     seconds++;
 
     //Logic to determine when to increment next value
@@ -32,37 +20,49 @@ function StopWatch() {
     }
 
     //If seconds/minutes are only one digit, add a leading 0
-    const getFormattedNumber = number => (number > 10 ? number : `0${number}`);
+    let getFormattedNumber = (number) => (number > 10 ? number : `0${number}`);
 
     //Display updated time values to user
-    counterElement.innerHTML = `${getFormattedNumber(minutes)}:${getFormattedNumber(seconds)}`;
+    counterElement.innerText = `${getFormattedNumber(minutes)}:${getFormattedNumber(seconds)}`;
 }
 
+//Define variables to hold setInterval()
+let interval = null;
 
-function startStop() {
+let status = Status_Types.STOPPED;
+
+const startStop = () => {
     if (status === Status_Types.STOPPED) {
 
         // Start the Stopwatch by calling setInterval() function
         interval = window.setInterval(StopWatch, 1000);
-        document.getElementById("startStop").innerHTML = "Stop";
-        status = 1;
+        startStopButton.innerHTML = Button_Labels.Start;
+        status = Status_Types.STARTED;
     }
 
     else {
         window.clearInterval(interval);
-        document.getElementById("startStop").innerHTML = "Start";
-        status = 0;
+        startStopButton.innerHTML = Button_Labels.Stop;
+        status = Status_Types.STOPPED;
     }
 }
 
 // Function to Reset StopWatch
-function reset() {
+const reset = () => {
     window.clearInterval("interval");
     seconds = 0;
     minutes = 0;
-    document.getElementById("counterElement").innerHTML = "00:00";
-    document.getElementById("startStop").innerHTML = "start";
+    counterElement.innerHTML = "00:00";
+    startStopButton.innerHTML = Button_Labels.Start;
 }
+
+// Calls the functions when the users clicks the buttons
+starTimerBtn.onclick = startStop;
+resetTimerBtn.onclick = reset;
+
+
+
+
 
 
 
